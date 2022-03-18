73b80ff40f0bf443f9c3d8d24efd6b5ba39203f1
Dindy Pierre <ing.pierredindy@gmail.com>
2022-03-18 21:50:27 +0100

    He indentado el archivo contacto.js.

--- js/contacto.js
+++ js/contacto.js
@@ -8,15 +8,14 @@ $("#formContact").submit(function (e) {
   .then(res => res.ok ? res.json(): Promise.reject(res))
   .then(json => {
     $("#formContact-contenedorLoader").fadeIn(1000);
-      $("#formContact-contenedorLoader").fadeOut(1000, function() {
+    $("#formContact-contenedorLoader").fadeOut(1000, function() {
       $("#formContact-notificacionEnvio").fadeIn(2000, function(){
-      $("#formContact-notificacionEnvio").fadeOut(2000, function(){
-        const formulario = document.getElementById("formContact");
-        formulario.reset();
+        $("#formContact-notificacionEnvio").fadeOut(2000, function(){
+          const formulario = document.getElementById("formContact");
+          formulario.reset();
+        })
       })
-    })
-  });
-    console.log(json)
+    });
   })
   .catch(err => {
     console.log(err);
